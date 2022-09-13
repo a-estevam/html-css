dbb8d0bdb2c6ea0960ee6de95eb486d2f277a720
a-estevam <estevam.adesign@gmail.com>
2022-09-13 17:18:35 -0300

    mobileMenu

---
+++ 01_TUTORIAL_ORIGAMID/navbar.js
@@ -0,0 +1,9 @@
+const btnMobile = document.getElementById("mobileMenu");
+
+function toggleMenu (){
+    const nav = document.getElementById("menu");
+    menu.classList.toggle('active'); 
+
+}
+
+btnMobile addEventListener('click'toggleMenu);
